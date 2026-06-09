886245b9eb951bac5c3c92248e5d5e7d7ef6e22a
maelp <mael.piquand8@etu.univ-lorraine.fr>
2026-06-09 17:04:13 +0200

    exercice 3

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
 (() => {
   // ts/config.ts
-  var api = "https://webetu.iutnc.univ-lorraine.fr/www/canals5/phox/api";
-  var config_default = api;
+  var apiRoot = "/www/canals5/phox/api";
+  var apiBase = "https://webetu.iutnc.univ-lorraine.fr";
+  var config_default = apiRoot;
 
   // ts/photoloader.ts
+  function buildUrl(uri) {
+    if (uri.startsWith("http://") || uri.startsWith("https://")) {
+      return uri;
+    }
+    if (uri.startsWith("/www/")) {
+      return apiBase + uri;
+    }
+    if (uri.startsWith("/")) {
+      return apiBase + config_default + uri;
+    }
+    return apiBase + config_default + "/" + uri;
+  }
   function loadPicture(idPicture) {
-    let url = config_default + "/photos/" + idPicture;
-    return fetch(url, { credentials: "include" }).then((response) => {
-      if (!response.ok) {
-        return Promise.reject(
-          new Error("Erreur HTTP : " + response.status)
-        );
-      }
-      return response.json();
-    }).catch((err) => {
-      if (err instanceof Error) {
-        console.log(err.message);
-      }
-      return Promise.reject(err);
-    });
+    return loadResource(`${config_default}/photos/${idPicture}`);
   }
   function loadResource(uri) {
-    return fetch("https://webetu.iutnc.univ-lorraine.fr" + uri, { credentials: "include" }).then((response) => {
+    return fetch(buildUrl(uri), {
+      credentials: "include"
+    }).then((response) => {
       if (!response.ok) {
         return Promise.reject(new Error(response.statusText));
       }
@@ -37,74 +39,123 @@
   // ts/ui.ts
   function displayPicture(data) {
     const templateElement = document.querySelector("#photoTemplate");
-    const templateSource = templateElement.innerHTML;
-    const template = Handlebars.compile(templateSource);
+    const template = Handlebars.compile(templateElement.innerHTML);
     const html = template(data);
     const photoSection = document.querySelector("#la_photo");
     photoSection.innerHTML = html;
   }
   function displayCategory(data) {
     const categorie = document.querySelector("#la_categorie");
-    console.log(data);
-    categorie.innerHTML = "categorie : " + data.categorie.nom;
+    const descr = document.querySelector("#categorie_descr");
+    categorie.textContent = `categorie : ${data.categorie.nom}`;
+    descr.textContent = data.categorie.descr;
   }
   function displayComments(data) {
     const liste = document.querySelector("#les_commentaires");
     liste.innerHTML = "";
     data.comments.forEach((comment) => {
-      liste.innerHTML += "<li>" + comment.pseudo + " : " + comment.content + "</li>";
+      const li = document.createElement("li");
+      li.textContent = `${comment.pseudo} : ${comment.content}`;
+      liste.appendChild(li);
     });
   }
 
   // ts/gallery.ts
+  var currentGallery = null;
+  function loadGalleryFromUri(uri) {
+    return loadResource(uri).then((gallery) => {
+      currentGallery = gallery;
+      return gallery;
+    });
+  }
   function load() {
-    return loadResource(
-      "/www/canals5/phox/api/photos"
-    );
+    return loadGalleryFromUri(`${config_default}/photos`);
+  }
+  function next() {
+    if (currentGallery === null) {
+      return load();
+    }
+    return loadGalleryFromUri(currentGallery.links.next.href);
+  }
+  function prev() {
+    if (currentGallery === null) {
+      return load();
+    }
+    return loadGalleryFromUri(currentGallery.links.prev.href);
+  }
+  function first() {
+    if (currentGallery === null) {
+      return load();
+    }
+    return loadGalleryFromUri(currentGallery.links.first.href);
+  }
+  function last() {
+    if (currentGallery === null) {
+      return load();
+    }
+    return loadGalleryFromUri(currentGallery.links.last.href);
   }
 
   // ts/gallery_ui.ts
-  function display_gallery(galerie) {
-    const source = document.querySelector("#galleryTemplate").innerHTML;
-    const template = Handlebars.compile(source);
-    const html = template(galerie);
-    document.querySelector("#les_photos").innerHTML = html;
+  function displayGallery(galerie) {
+    console.log(galerie.photos[0]);
+    const templateElement = document.querySelector("#galleryTemplate");
+    const template = Handlebars.compile(templateElement.innerHTML);
+    const html = template({ photos: galerie.photos });
+    const container = document.querySelector("#les_photos");
+    container.innerHTML = html;
   }
 
   // ts/index.ts
-  var boutonload = document.querySelector("#load");
-  boutonload.addEventListener(
-    "click",
-    () => {
-      load().then((galerie) => {
-        display_gallery(galerie);
-      });
+  function handleError(err) {
+    if (err instanceof Error) {
+      console.log(err.message);
     }
-  );
-  function getPicture(id) {
-    loadPicture(id).then((data) => {
-      displayPicture(data);
-      getCategory(data).then((categorie) => {
-        displayCategory(categorie);
-      });
-      getComments(data).then((comments) => {
-        displayComments(comments);
-      });
-    }).catch((err) => {
-      if (err instanceof Error) {
-        console.log(err.message);
-      }
-    });
   }
-  getPicture(
-    window.location.hash ? Number(window.location.hash.substr(1)) : 105
-  );
   function getCategory(data) {
-    console.log(data.links.categorie.href);
     return loadResource(data.links.categorie.href);
   }
   function getComments(data) {
     return loadResource(data.links.comments.href);
   }
+  function getPicture(id) {
+    loadPicture(id).then((data) => {
+      displayPicture(data);
+      getCategory(data).then((categorie) => {
+        displayCategory(categorie);
+      }).catch(handleError);
+      getComments(data).then((comments) => {
+        displayComments(comments);
+      }).catch(handleError);
+    }).catch(handleError);
+  }
+  function refreshGallery(action) {
+    action().then((galerie) => {
+      displayGallery(galerie);
+    }).catch(handleError);
+  }
+  var boutonLoad = document.querySelector("#load");
+  var boutonNext = document.querySelector("#next");
+  var boutonPrev = document.querySelector("#prev");
+  var boutonFirst = document.querySelector("#first");
+  var boutonLast = document.querySelector("#last");
+  boutonLoad.addEventListener("click", () => {
+    refreshGallery(load);
+  });
+  boutonNext.addEventListener("click", () => {
+    refreshGallery(next);
+  });
+  boutonPrev.addEventListener("click", () => {
+    refreshGallery(prev);
+  });
+  boutonFirst.addEventListener("click", () => {
+    refreshGallery(first);
+  });
+  boutonLast.addEventListener("click", () => {
+    refreshGallery(last);
+  });
+  getPicture(
+    window.location.hash ? Number(window.location.hash.slice(1)) : 105
+  );
 })();
 //# sourceMappingURL=index.js.map
